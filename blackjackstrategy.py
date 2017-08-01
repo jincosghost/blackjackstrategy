125a1f2b34e2916d08315d13077f29ee25d1c6a8
jincosghost <phaposlucl@thraml.com>
2017-08-01 08:14:28 +0100

    0.0.2 Thanks to @meh

--- blackjackstrategy.py
+++ blackjackstrategy.py
@@ -7,7 +7,8 @@ import sys
 PLAYING = True
 
 
-class colour:
+class Colour:
+    """Define text colours"""
     PURPLE = '\033[95m'
     CYAN = '\033[96m'
     DARKCYAN = '\033[36m'
@@ -21,6 +22,7 @@ class colour:
 
 
 def cleanscreen():
+    """ Clear the screen"""
     if os.name == "nt":
         os.system('cls')
     else:
@@ -31,121 +33,109 @@ def cleanscreen():
 while PLAYING:
     cleanscreen()
 
-    if (sys.version_info < (3, 0)):
+    if sys.version_info < (3, 0):
         print(
-            colour.RED +
-            "This app requires Python 3! Please reload with: python3 perma.py" +
-            colour.END)
+            Colour.RED +
+            "This app requires Python 3! Please reload with: python3 blackjackstrategy.py"
+            + Colour.END)
         sys.exit(1)
 
-    A = True
-    B = False
-    C = False
+    SPLIT = Colour.BLUE + "SPLIT" + Colour.END
+    HIT = Colour.GREEN + "HIT" + Colour.END
+    STAND = Colour.YELLOW + "STAND" + Colour.END
+    DOUBLE = Colour.PURPLE + "DOUBLE" + Colour.END
 
-    SPLIT = colour.BLUE + "SPLIT" + colour.END
-    HIT = colour.GREEN + "HIT" + colour.END
-    STAND = colour.YELLOW + "STAND" + colour.END
-    DOUBLE = colour.PURPLE + "DOUBLE" + colour.END
+    DEALER = int(input('What is the dealers card? (Ace = 1)\n'))
 
-    while A:
-        DEALER = int(input('What is the dealers card? (Ace = 1)\n'))
+    while DEALER > 10 or DEALER < 1:
+        DEALER = int(
+            input(
+                Colour.RED +
+                "Enter a number between 1 and 10. Ace = 1.\n" +
+                Colour.END))
 
-        if DEALER > 10 or DEALER < 1:
-            print(
-                colour.RED +
-                "Enter a number between 1 and 10. Ace = 1." +
-                colour.END)
-        else:
-            A = False
-            B = True
+    CARD1 = int(input('What is your first card? (Ace = 1)\n'))
 
-    while B:
-        CARD1 = int(input('What is your first card? (Ace = 1)\n'))
+    while CARD1 > 10 or CARD1 < 1:
+        CARD1 = int(
+            input(
+                Colour.RED +
+                "Enter a number between 1 and 10. Ace = 1.\n" +
+                Colour.END))
 
-        if CARD1 > 10 or CARD1 < 1:
-            print(
-                colour.RED +
-                "Enter a number between 1 and 10. Ace = 1." +
-                colour.END)
-        else:
-            B = False
-            C = True
+    CARD2 = int(input('What is your second card? (Ace = 1)\n'))
 
-    while C:
-        CARD2 = int(input('What is your second card? (Ace = 1)\n'))
+    while CARD2 > 10 or CARD2 < 1:
+        CARD2 = int(
+            input(
+                Colour.RED +
+                "Enter a number between 1 and 10. Ace = 1.\n" +
+                Colour.END))
 
-        if CARD2 > 10 or CARD2 < 1:
-            print(
-                colour.RED +
-                "Enter a number between 1 and 10. Ace = 1." +
-                colour.END)
-        else:
-            C = False
-
-        if CARD1 == CARD2 and CARD2 == 1 or CARD2 == 8:
+    if CARD1 == CARD2 == 1 or CARD2 == 8:
+        print(SPLIT)
+    elif CARD1 == CARD2 == 10:
+        print(STAND)
+    elif CARD1 == CARD2 == 2 or CARD2 == 3 or 7:
+        if 2 <= DEALER <= 7:
             print(SPLIT)
-        elif CARD1 == CARD2 and CARD2 == 10:
+        else:
+            print(HIT)
+    elif CARD1 == CARD2 == 4:
+        if DEALER == 5 or 6:
+            print(SPLIT)
+        else:
+            print(HIT)
+    elif CARD1 == CARD2 == 5:
+        if DEALER == 1 or 10:
+            print(HIT)
+        else:
+            print(DOUBLE)
+    elif CARD1 == CARD2 == 6:
+        if DEALER <= 6:
+            print(SPLIT)
+        else:
+            print(HIT)
+    elif CARD1 == CARD2 == 7:
+        if DEALER <= 7:
+            print(SPLIT)
+        else:
+            print(HIT)
+    elif CARD1 == CARD2 == 8:
+        if DEALER <= 7:
+            print(SPLIT)
+        else:
+            print(HIT)
+    elif CARD1 == CARD2 == 9:
+        if DEALER == 7 or 10 or 1:
             print(STAND)
-        elif CARD1 == CARD2 and CARD2 == 2 or CARD2 == 3 or CARD2 == 7:
-            if DEALER >= 2 and DEALER <= 7:
-                print(SPLIT)
-            else:
-                print(HIT)
-        elif CARD1 == CARD2 and CARD2 == 4:
+        else:
+            print(SPLIT)
+    elif CARD1 == 1 or CARD2 == 1:
+        if CARD1 >= 8 or CARD2 >= 8:
+            print(STAND)
+        elif CARD1 == 2 or 3 or CARD2 == 2 or 3:
             if DEALER == 5 or DEALER == 6:
-                print(SPLIT)
-            else:
-                print(HIT)
-        elif CARD1 == CARD2 and CARD2 == 5:
-            if DEALER == 1 or DEALER == 10:
-                print(HIT)
-            else:
                 print(DOUBLE)
-        elif CARD1 == CARD2 and CARD2 == 6:
-            if DEALER <= 6:
-                print(SPLIT)
             else:
                 print(HIT)
-        elif CARD1 == CARD2 and CARD2 == 7:
-            if DEALER <= 7:
-                print(SPLIT)
+        elif CARD1 == 4 or CARD1 == 5 or CARD2 == 4 or CARD2 == 5:
+            if DEALER >= 4 and DEALER <= 6:
+                print(DOUBLE)
             else:
                 print(HIT)
-        elif CARD1 == CARD2 and CARD2 == 8:
-            if DEALER <= 7:
-                print(SPLIT)
+        elif CARD1 == 6 or CARD2 == 6:
+            if DEALER >= 3 and DEALER <= 6:
+                print(DOUBLE)
             else:
                 print(HIT)
-        elif CARD1 == CARD2 and CARD2 == 9:
-            if DEALER == 7 or DEALER == 10 or DEALER == 1:
+        elif CARD1 == 7 or CARD2 == 7:
+            if 3 <= DEALER <= 6:
+                print(DOUBLE)
+            elif DEALER == 2 or 7 or 8:
                 print(STAND)
             else:
-                print(SPLIT)
-        elif CARD1 == 1 or CARD2 == 1:
-            if CARD1 >= 8 or CARD2 >= 8:
-                print(STAND)
-            elif CARD1 == 2 or CARD1 == 3 or CARD2 == 2 or CARD2 == 3:
-                if DEALER == 5 or DEALER == 6:
-                    print(DOUBLE)
-                else:
-                    print(HIT)
-            elif CARD1 == 4 or CARD1 == 5 or CARD2 == 4 or CARD2 == 5:
-                if DEALER >= 4 and DEALER <= 6:
-                    print(DOUBLE)
-                else:
-                    print(HIT)
-            elif CARD1 == 6 or CARD2 == 6:
-                if DEALER >= 3 and DEALER <= 6:
-                    print(DOUBLE)
-                else:
-                    print(HIT)
-            elif CARD1 == 7 or CARD2 == 7:
-                if DEALER >= 3 and DEALER <= 6:
-                    print(DOUBLE)
-                elif DEALER == 2 or DEALER == 7 or DEALER == 8:
-                    print(STAND)
-                else:
-                    print(HIT)
+                print(HIT)
         else:
             CARDX = CARD1 + CARD2
             if CARDX <= 8:
@@ -153,22 +143,22 @@ while PLAYING:
             elif CARDX >= 17:
                 print(STAND)
             elif CARDX == 9:
-                if DEALER >= 3 and DEALER <= 6:
+                if 3 <= DEALER <= 6:
                     print(DOUBLE)
                 else:
                     print(HIT)
             elif CARDX == 10:
-                if DEALER >= 2 and DEALER <= 9:
+                if 2 <= DEALER <= 9:
                     print(DOUBLE)
                 else:
                     print(HIT)
             elif CARDX == 11:
-                if DEALER >= 2 and DEALER <= 10:
+                if 2 <= DEALER <= 10:
                     print(DOUBLE)
                 else:
                     print(HIT)
             elif CARDX == 12:
-                if DEALER >= 4 and DEALER <= 6:
+                if 4 <= DEALER <= 6:
                     print(STAND)
                 else:
                     print(HIT)
@@ -178,11 +168,16 @@ while PLAYING:
                 else:
                     print(HIT)
 
-    PLAYING = False
+    REPEAT = input('\nRepeat? y/n\n').lower()
+    if REPEAT != "y":
+        cleanscreen()
+        PLAYING = False
+    else:
+        cleanscreen()
 
 __author__ = "Jinco"
 __copyright__ = "Copyright 2017, Jinco"
 __license__ = "GPL-3.0"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__ = "Jinco"
 __status__ = "Development"
